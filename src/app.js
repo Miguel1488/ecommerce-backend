2e751cdb2a92b7e7567137817ce3a81f8839573e
Miguel1488 <maespinozav@gmail.com>
2023-04-09 22:38:09 -0500

    document

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('../swagger.json');
 require('dotenv').config();
 const db = require("./utils/database");
 const cors = require("cors");
@@ -9,12 +11,6 @@ const erroHandlerRouter = require("./routes/errorhandlers.routes");
 const productRouter = require("./routes/product.routes");
 const orderRouter = require("./routes/order.routes");
 const cartRouter = require("./routes/cart.routes");
-
-
-// const inorderRouter = require("./routes/product_in_order.routes");
-// const producRouter = require("./routes/product_in_cart.routes");
-
-
 
 
 
@@ -37,6 +33,8 @@ db.sync({ alter: false }) // alterar los atributos
     .then(() => console.log("Base de datos sync"))
     .catch((error) => console.log(error));
 
+
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(userRouter);
 app.use(productRouter);
 app.use(orderRouter);
